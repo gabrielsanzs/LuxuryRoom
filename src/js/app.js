f7b9483f4e9b960212901bb60c0f3badaf80139c
Gabriel Sanchez <88407659+gabrielsanzs@users.noreply.github.com>
2022-11-24 03:25:40 -0300

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,6 @@ class MobileNavbar {
       this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
-  
       this.handleClick = this.handleClick.bind(this);
     }
   
